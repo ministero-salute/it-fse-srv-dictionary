d3207f4e58447ad34714d07e0a3c68134653090b
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2022-09-29 20:03:05 +0200

    feat(ResponseCTL) : Remove transaction id from res terminology

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/controller/impl/TerminologyCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/controller/impl/TerminologyCTL.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import it.finanze.sanita.fse2.ms.edssrvdictionary.config.Constants;
-import it.finanze.sanita.fse2.ms.edssrvdictionary.config.ValidationCFG;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.controller.AbstractCTL;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.controller.ITerminologyCTL;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.dto.TerminologyDocumentDTO;
@@ -18,13 +17,11 @@ import it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response.GetTerminologyRes
 import it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response.TerminologyResponseDTO;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.enums.OperationLogEnum;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.enums.ResultLogEnum;
-import it.finanze.sanita.fse2.ms.edssrvdictionary.enums.UIDModeEnum;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.exceptions.DocumentAlreadyPresentException;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.exceptions.DocumentNotFoundException;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.exceptions.OperationException;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.logging.ElasticLoggerHelper;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.service.ITerminologySRV;
-import it.finanze.sanita.fse2.ms.edssrvdictionary.utility.StringUtility;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -48,18 +45,12 @@ public class TerminologyCTL extends AbstractCTL implements ITerminologyCTL{
     @Autowired
     private ITerminologySRV terminologySRV;
 
-	@Autowired
-	private ValidationCFG validationCFG;
-
 
     @Override
 	public GetTerminologyResDTO getTerminologyById(HttpServletRequest request,  String id) throws OperationException, DocumentNotFoundException {
-
 		log.info(Constants.Logs.CALLED_GET_TERMINOLOGY_BY_ID); 
 		elasticLogger.info(Constants.Logs.CALLED_GET_TERMINOLOGY_BY_ID, OperationLogEnum.QUERY_TERMINOLOGY_BY_ID, ResultLogEnum.OK, new Date()); 
-
 		TerminologyDocumentDTO doc = terminologySRV.findById(id); 
-
 		return new GetTerminologyResDTO(getLogTraceInfo(), doc);
 
 	}
@@ -67,12 +58,8 @@ public class TerminologyCTL extends AbstractCTL implements ITerminologyCTL{
 
 	@Override
 	public TerminologyResponseDTO uploadTerminologyFile(HttpServletRequest request, MultipartFile file) throws IOException, OperationException, DocumentAlreadyPresentException, DocumentNotFoundException {
-		
-		final String transactionId = StringUtility.generateTransactionUID(UIDModeEnum.get(validationCFG.getTransactionIDStrategy())); 
-
 		terminologySRV.uploadTerminologyFile(file);
-
-		return new TerminologyResponseDTO(getLogTraceInfo(), transactionId);
+		return new TerminologyResponseDTO(getLogTraceInfo());
 		
 	}
     
--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/dto/response/TerminologyResponseDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/dto/response/TerminologyResponseDTO.java
@@ -1,10 +1,5 @@
 package it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response;
 
-import static it.finanze.sanita.fse2.ms.edssrvdictionary.utility.ValidationUtility.DEFAULT_STRING_MAX_SIZE;
-import static it.finanze.sanita.fse2.ms.edssrvdictionary.utility.ValidationUtility.DEFAULT_STRING_MIN_SIZE;
-
-import javax.validation.constraints.Size;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -25,16 +20,13 @@ public class TerminologyResponseDTO extends ResponseDTO {
 	 */
 	private static final long serialVersionUID = 5857199886068379718L;
 
-	@Size(min = DEFAULT_STRING_MIN_SIZE, max = DEFAULT_STRING_MAX_SIZE)
-	private String transactionId;
 
 	public TerminologyResponseDTO() {
 		super();
 	}
 
-	public TerminologyResponseDTO(final LogTraceInfoDTO traceInfo, final String inTransactionId) {
+	public TerminologyResponseDTO(final LogTraceInfoDTO traceInfo) {
 		super(traceInfo);
-		transactionId = inTransactionId;
 	}
 	
 }
