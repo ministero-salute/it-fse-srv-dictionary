1a42cd1e798517fdf77db00334507c03c198028d
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-12-14 14:31:02 +0100

    fix(Swagger): Display list as array

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/dto/response/crud/GetTermsPageResDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/dto/response/crud/GetTermsPageResDTO.java
@@ -4,6 +4,7 @@
 package it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response.crud;
 
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.dto.TerminologyDocumentDTO;
 import it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response.base.ResponseDTO;
@@ -22,7 +23,7 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 public class GetTermsPageResDTO extends ResponseDTO {
 
-    @Schema(implementation = TerminologyDocumentDTO.class)
+    @ArraySchema(schema = @Schema(implementation = TerminologyDocumentDTO.class))
     private List<TerminologyDocumentDTO> items;
     private GetTermsPageLinksDTO links;
     private long numberOfItems;
