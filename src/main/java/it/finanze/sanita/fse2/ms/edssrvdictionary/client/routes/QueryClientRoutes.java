a50e6d82730a968c260e5c9684af7966bfa25131
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-06-23 17:36:09 +0200

    fix: date format for null

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/client/routes/QueryClientRoutes.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/client/routes/QueryClientRoutes.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.Nullable;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 import static it.finanze.sanita.fse2.ms.edssrvdictionary.client.routes.base.ClientRoutes.Query.*;
+import static java.time.ZoneOffset.UTC;
 
 
 @Component
@@ -23,7 +26,7 @@ public final class QueryClientRoutes {
     public String history(Date lastUpdate) {
         return base()
             .pathSegment(API_VERSION, HISTORY_PATH)
-            .queryParam(LAST_UPDATE_QP, lastUpdate)
+            .queryParam(LAST_UPDATE_QP, asTimestamp(lastUpdate))
             .build()
             .toUriString();
     }
@@ -33,6 +36,10 @@ public final class QueryClientRoutes {
             .pathSegment(API_VERSION, HISTORY_PATH, RESOURCE_PATH, resourceId, versionId)
             .build()
             .toUriString();
+    }
+
+    private static String asTimestamp(@Nullable Date lastUpdate) {
+        return lastUpdate == null ? "" : ZonedDateTime.ofInstant(lastUpdate.toInstant(), UTC).toString();
     }
 
 }
