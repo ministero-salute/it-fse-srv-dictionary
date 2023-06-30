a8fe843001c042f5081c03ab98e48def02eea1a6
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-06-30 13:38:46 +0200

    fix: format date according to iso standard

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/repository/entity/resources/base/IndexMetaETY.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/repository/entity/resources/base/IndexMetaETY.java
@@ -1,5 +1,6 @@
 package it.finanze.sanita.fse2.ms.edssrvdictionary.repository.entity.resources.base;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,9 +12,12 @@ import static it.finanze.sanita.fse2.ms.edssrvdictionary.dto.response.changes.qu
 @AllArgsConstructor
 public class IndexMetaETY {
 
+    private static final String PTT_ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
     String oid;
     String version;
     String type;
+    @JsonFormat(pattern = PTT_ISO_8601)
     Date released;
     boolean whitelist;
 
