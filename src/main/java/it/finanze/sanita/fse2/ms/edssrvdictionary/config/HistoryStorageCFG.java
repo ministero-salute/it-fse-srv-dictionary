46af59803afbe6c726977963ae720ede0bc7cf88
Simone Le Noci <72795995+SimoneNuts@users.noreply.github.com>
2023-07-10 17:58:12 +0200

    fix: historystoragecfg

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/config/HistoryStorageCFG.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdictionary/config/HistoryStorageCFG.java
@@ -1,12 +1,15 @@
 package it.finanze.sanita.fse2.ms.edssrvdictionary.config;
 
-import it.finanze.sanita.fse2.ms.edssrvdictionary.service.IChangeSetSRV;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 
+import it.finanze.sanita.fse2.ms.edssrvdictionary.service.IChangeSetSRV;
+
 @Configuration
+@Profile("!" + Constants.Profile.TEST)
 public class HistoryStorageCFG {
 
     @Autowired
